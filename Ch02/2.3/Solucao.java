6a0e70faae659da9b019ce78904bee8bdfb17c29
Silvaldo Stolarz <59754097+SiSt12@users.noreply.github.com>
2025-06-12 15:29:57 -0300

    Ch02. Ex.1,2,3

---
+++ Ch02/2.3/Solucao.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void Solucao(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int c;
+		int thisIsAVariable;
+		int q76354;
+		int number;
+		
+		System.out.print("Digite um inteiro: ");
+		value = input.nextInt();
+		System.out.println("This is a java program");
+		System.out.printf("%s%n%s%n", "This is a java", "program");
+		
+		if (value != 7)
+			System.out.println("The variable number is not equal to 7");
+		
+		input.close()
+	}
+}
