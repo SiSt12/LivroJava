4a10ab8d10a73b61d786dd4978f8f28ecd172d47
Silvaldo Stolarz <59754097+SiSt12@users.noreply.github.com>
2025-07-08 09:05:01 -0300

    Add files via upload

---
+++ Ch04/4.15/src/Main.java
@@ -0,0 +1,32 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int age;
+		if (age >= 65) {
+		 System.out.println("Age is greater than or equal to 65");
+		}
+		else {
+		 System.out.println("Age is less than 65)");
+		}
+		 
+		 int x = 1, total = 0;
+		 while (x <= 10)
+		 {
+		  total += x;
+		  ++x;
+		 }
+		 
+		 while (x <= 100)
+			 total += x;
+			 ++x;
+		
+		 int y = 1;
+		 while (y > 0) {
+			 System.out.println(y);
+			 ++y;
+		 }
+	}
+
+}
