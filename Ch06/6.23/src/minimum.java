127e645b43da8f9b853631b8321c932f49100e53
Silvaldo Stolarz <59754097+SiSt12@users.noreply.github.com>
2025-07-10 11:00:00 -0300

    Add files via upload

---
+++ Ch06/6.23/src/minimum.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class minimum {
+	public static double minimum3(double num[]) { 
+		double primeiroMin = Math.min(num[0], num[1]);
+		double minFinal = Math.min(num[2], primeiroMin);
+		return minFinal;
+	}
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		double[] num = new double[3];
+		
+		for (int i = 0; i <= 2; i++) {
+			System.out.printf("Digite o %dº número: ", i + 1);
+			num[i] = input.nextDouble();
+		}
+		
+		double numMinimo = minimum3(num);
+		System.out.println("O numero minimo é: " + numMinimo);
+		
+	}
+	
+}
